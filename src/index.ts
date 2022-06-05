d589dfa169132794e92a95543412d9772825d208
Anxo Rodriguez <anxolin@gmail.com>
2022-06-05 14:18:42 +0100

    Get comments

--- src/index.ts
+++ src/index.ts
@@ -1,7 +1,11 @@
 import { request, gql } from 'graphql-request'
+import axios from 'axios'
 
 
-const SUBGRAPH_URL = 'https://gateway.thegraph.com/api/47e8db8d0f1b4a6c98b190e04f20dcb1/subgraphs/id/0x0503024fcc5e1bd834530e69d592dbb6e8c03968-0'
+const MARKET_SUBGRAPH_URL = 'https://gateway.thegraph.com/api/47e8db8d0f1b4a6c98b190e04f20dcb1/subgraphs/id/0x0503024fcc5e1bd834530e69d592dbb6e8c03968-0'
+const IPFS_3BOX_SPACE = 'did%3A3%3Abafyreiejas3ngkhuqdpv46vbcdhhxnfg6ubr5qlofbgz3h2ky55wf6vewa'
+
+const getCommentsUrl = (market: string) => `https://ipfs.3box.io/thread?space=conditional_exchange&name=${market}&mod=${IPFS_3BOX_SPACE}&members=false`
 
 const MARKETS = [
   '0x95b2271039b020aba31b933039e042b60b063800',  // Will Joe Biden win the 2020 United States presidential election? (DAI)
@@ -9,10 +13,11 @@ const MARKETS = [
   '0xffbc624070cb014420a6f7547fd05dfe635e2db2', // Will there be a day with at least 1000 reported Corona death in the US in the first 14 days of July?
 ]
 
+
 // GetMarket
 // 0x00031c96aae99eb00376e273e10c82d0ee77305d
 
-const query = gql`
+const getMarketQuery = gql`
   query GetMarket($id: ID!) {
     fixedProductMarketMaker(id: $id) {
       id
@@ -36,10 +41,23 @@ const query = gql`
   }
 `
 
-async function fetchMarketInfo(marketId: string) {
+const getQuestion = gql`
+{
+  query GetQuestion($id: ID!) {
+    question(id: $id) {
+      indexedFixedProductMarketMakers {
+        id
+        collateralToken
+      }
+    }
+  }
+}
+`
+
+async function getMarketInfo(marketId: string) {
   const market = await request({
-    url: SUBGRAPH_URL,
-    document: query,
+    url: MARKET_SUBGRAPH_URL,
+    document: getMarketQuery,
     variables: {
       id: marketId
     }
@@ -48,13 +66,21 @@ async function fetchMarketInfo(marketId: string) {
   return market
 }
 
+
+async function getComments(marketId: string) {
+  const commentsUrl = getCommentsUrl(marketId)
+  return axios.get(commentsUrl)
+}
+
 async function main() {
   console.log('Markets', MARKETS)
 
-  const marketId = MARKETS[0]
-  const marketInfo = await fetchMarketInfo(marketId)
+  const marketId = MARKETS[1]
+  // const marketInfo = await getMarketInfo(marketId)
+  const comments = await getComments(marketId)
 
-  console.log('Market info: ', marketInfo)
+  // console.log('Market info: ', marketInfo)
+  console.log('Comments: ', comments)
 }
 
 main().catch(console.error)
